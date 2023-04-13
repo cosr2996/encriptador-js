2f235223611b6eff493e3c402eec9d0292c16ff5
cosr2996 <115452805+cosr2996@users.noreply.github.com>
2023-04-13 02:09:56 -0600

    se corrigieron aspectos de diseno y se agrego la funcionalidad de copiar el texto al precionar el boton de copiar

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
 'use strict'
-
+//boton de encriptar
 var encriptar = document.getElementById('btn-en');
 encriptar.addEventListener("click", encriptarTexto);
-
+//boton de desencriptar
 var desencriptar = document.getElementById('btn-des');
 desencriptar.addEventListener("click", desencriptarTexto);
+// text area de entrada
+var result = document.getElementById('message-result');
+//imgagen de resultado
+var image = document.getElementById('vacio');
+//booton copiar
+var copy = document.getElementById('copiar');
+copy.addEventListener("click", copyText);
 
 
-
-
+//eccnripta una cadena de texto 
 function encriptarTexto(){
     var message = document.getElementById("message").value;
     var cadena=[];
@@ -33,36 +39,34 @@ function encriptarTexto(){
             cadena.push(message.charAt(i));
         }}
         cadena = cadena.join('')
-        console.log(cadena);
+
+        result.style.display ="block";
+        image.style.display ="none";
+        result.value=cadena;
+        
     }
 
+    //desencripta una cadena de texto encriptada
+     function desencriptarTexto(){
 
-    // function desencriptarTexto(){
-    //     var message = document.getElementById("message").value;
-    // var cadena=[];
+     }
 
-    // for(var i=0 ; i<=message.length-1 ;i++){
+
+
+     //copia el texto en portapapeles
+     async function copyText(){
         
-    //     if(message.charAt(i) == 'ai'){
-    //            cadena.push('a');
-    //     }
-    //     else if(message.charAt(i) == 'enter'){
-    //            cadena.push('e');
-    //     }
-    //     else if(message.charAt(i) == 'imes'){
-    //            cadena.push('i');
-    //     }
-    //     else if(message.charAt(i) == 'ober'){
-    //            cadena.push('o');
-    //     }
-    //     else if(message.charAt(i) == 'ufat'){
-    //            cadena.push('u');
-    //     }else{
-    //         cadena.push(message.charAt(i));
-    //     }}
-    //     cadena = cadena.join('')
-    //     console.log(cadena);
-    // }
+        try{
+           await navigator.clipboard.writeText(result.value)   
+        }
+        catch(err){
+            console.log(err)
+        }
+        
+        
+
+     }
 
    
     
+   
