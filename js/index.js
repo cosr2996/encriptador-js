0dd7a0f3e1513219e00e665bbe009bfe758a81ce
cosr2996 <115452805+cosr2996@users.noreply.github.com>
2023-04-14 19:00:09 -0600

    se mejoro el codigo, se completo el responsive

--- js/index.js
+++ js/index.js
@@ -1,77 +1,102 @@
 'use strict'
 
-       //boton de encriptar
-       var encriptar = document.getElementById('btn-en');
-       encriptar.addEventListener("click", encriptarTexto);
-       //boton de desencriptar
-       var desencriptar = document.getElementById('btn-des');
-       desencriptar.addEventListener("click", desencriptarTexto);
-       // text area de entrada
-       var result = document.getElementById('message-result');
-       //imgagen de resultado
-       var image = document.getElementById('vacio');
-       //booton copiar
-       var copy = document.getElementById('copiar');
-       copy.addEventListener("click", copyText);
+//boton de encriptar
+var btn_encriptar = document.getElementById('btn-en');
+btn_encriptar.addEventListener("click", encriptarTexto);
+//boton de desencriptar
+var btn_desencriptar = document.getElementById('btn-des');
+btn_desencriptar.addEventListener("click", desencriptarTexto);
+// text area de entrada
+var area_result = document.getElementById('message-result');
+//imgagen de resultado
+var image = document.getElementById('vacio');
+//booton copiar
+var btn_copy = document.getElementById('copiar');
+btn_copy.addEventListener("click", copyText);
+
+var logo = document.getElementById('logo');
+logo.addEventListener('click', clear);
 
 
-       //eccnripta una cadena de texto 
-       function encriptarTexto() {
-              var message = document.getElementById("message").value;
-              var cadena = [];
+//eccnripta una cadena de texto 
+function encriptarTexto() {
+       var message = document.getElementById("message").value;
+       var cadena = [];
 
-              for (var i = 0; i <= message.length - 1; i++) {
+       for (var i = 0; i <= message.length - 1; i++) {
 
-                     if (message.charAt(i) == 'a') {
-                            cadena.push('ai');
-                     }
-                     else if (message.charAt(i) == 'e') {
-                            cadena.push('enter');
-                     }
-                     else if (message.charAt(i) == 'i') {
-                            cadena.push('imes');
-                     }
-                     else if (message.charAt(i) == 'o') {
-                            cadena.push('ober');
-                     }
-                     else if (message.charAt(i) == 'u') {
-                            cadena.push('ufat');
-                     } else {
-                            cadena.push(message.charAt(i));
-                     }
+              if (message.charAt(i) == 'a') {
+                     cadena.push('ai');
               }
-              var texto = cadena.join('')
-
-              result.style.display = "block";
-              image.style.display = "none";
-              result.value = texto;
-
-       }
-
-       //desencripta una cadena de texto encriptada
-       function desencriptarTexto() {
-              var texto = document.getElementById("message").value;
-
-              var texto2 = texto.replaceAll("ober", 'o');
-              var texto3 = texto2.replaceAll("enter", "e");
-              var texto4 = texto3.replaceAll("ai", "a");
-              var texto5 = texto4.replaceAll("ufat", "u");
-              var texto6 = texto5.replaceAll("imes", "i");
-
-
-              result.style.display = "block";
-              image.style.display = "none";
-              result.value = texto6;
-
-       }
-
-       //copia el texto en portapapeles
-       async function copyText() {
-              try {
-                     await navigator.clipboard.writeText(result.value)
+              else if (message.charAt(i) == 'e') {
+                     cadena.push('enter');
               }
-              catch (err) {
-                     console.log(err)
+              else if (message.charAt(i) == 'i') {
+                     cadena.push('imes');
+              }
+              else if (message.charAt(i) == 'o') {
+                     cadena.push('ober');
+              }
+              else if (message.charAt(i) == 'u') {
+                     cadena.push('ufat');
+              } else {
+                     cadena.push(message.charAt(i));
               }
        }
+       var texto = cadena.join('')
+
+       area_result.style.display = "block";
+       btn_copy.style.display = "block";
+       image.style.display = "none";
+       area_result.value = texto;
+
+
+       if (screen.width < 923) {
+              area_result.style.height = `${area_result.scrollHeight}px`
+       }
+
+}
+
+//desencripta una cadena de texto encriptada
+function desencriptarTexto() {
+
+       var texto = document.getElementById("message").value;
+
+       texto = texto.replaceAll("ober", 'o');
+       texto = texto.replaceAll("enter", "e");
+       texto = texto.replaceAll("ai", "a");
+       texto = texto.replaceAll("ufat", "u");
+       texto = texto.replaceAll("imes", "i");
+
+       area_result.style.display = "block";
+       image.style.display = "none";
+       btn_copy.style.display = "block";
+       area_result.value = texto;
+
+       if (screen.width < 923) {
+              area_result.style.height = `${area_result.scrollHeight}px`
+       }
+
+}
+
+//copia el texto en portapapeles
+async function copyText() {
+       try {
+              await navigator.clipboard.writeText(area_result.value)
+       }
+       catch (err) {
+              console.log(err)
+       }
+}
+
+function clear() {
+       area_result.value = "";
+       document.getElementById("message").value = "";
+
+       if (screen.width <= 925) {
+              btn_copy.style.display = "none";
+              area_result.style.height = `100px`
+       }
+
+}
 
